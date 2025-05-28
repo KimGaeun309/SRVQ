6b9539cfd6280d04e4d170483ad6a7ffe80f3949
KimGaeun309 <gaeun8389@gmail.com>
2025-05-28 07:51:57 +0000

    ?

--- model/loss.py
+++ model/loss.py
@@ -184,16 +184,16 @@ class FastSpeech2Loss(nn.Module):
         total_style_loss = style_loss + guided_loss
 
         
-        classifier_loss = torch.zeros_like(mel_loss)
-        # for min_encoding_indice in min_encoding_indices:
-        orig_style_ref_embs = [orig_style_ref_embs[:, :256], orig_style_ref_embs[:, 256:512], orig_style_ref_embs[:, 256:384], orig_style_ref_embs[:, 384:512], orig_style_ref_embs[:, 512:]]
+        # classifier_loss = torch.zeros_like(mel_loss)
+        # # for min_encoding_indice in min_encoding_indices:
+        # orig_style_ref_embs = [orig_style_ref_embs[:, :256], orig_style_ref_embs[:, 256:512], orig_style_ref_embs[:, 256:384], orig_style_ref_embs[:, 384:512], orig_style_ref_embs[:, 512:]]
         
-        for style_ref_emb in orig_style_ref_embs:
-            emotions = inputs[3]
-            anchor, positive, negative = create_triplet_samples(style_ref_emb, emotions)
+        # for style_ref_emb in orig_style_ref_embs:
+        #     emotions = inputs[3]
+        #     anchor, positive, negative = create_triplet_samples(style_ref_emb, emotions)
 
-            if anchor is not None:
-                classifier_loss += self.triplet_margin_loss_fn(anchor, positive, negative) * 0.5
+        #     if anchor is not None:
+        #         classifier_loss += self.triplet_margin_loss_fn(anchor, positive, negative) * 0.5
             
             # # Clssifier
             # emotions_pred = min_encoding_indice
@@ -242,13 +242,13 @@ class FastSpeech2Loss(nn.Module):
         #     # print("cls_loss", classifier_loss)
 
         
-        style_consistency_loss = torch.tensor(0.0).to(mel_targets.device)
-        if style_reconstructed is not None:
-            style_consistency_loss = self.mae_loss(style_reconstructed, style_pred_embs.detach()) * 10
+        # style_consistency_loss = torch.tensor(0.0).to(mel_targets.device)
+        # if style_reconstructed is not None:
+        #     style_consistency_loss = self.mae_loss(style_reconstructed, style_pred_embs.detach()) * 10
         
-        total_loss = (
-            mel_loss + postnet_mel_loss + duration_loss + pitch_loss + energy_loss + total_style_loss + vq_loss + classifier_loss + style_consistency_loss
-        )
+        # mel_loss + postnet_mel_loss + duration_loss + pitch_loss + energy_loss + total_style_loss + vq_loss + classifier_loss +
+        total_loss = style_consistency_loss
+        
         return (
             total_loss,
             mel_loss,
